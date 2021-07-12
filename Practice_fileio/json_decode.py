05029b6565a08dcf8e9dfdaccbc3927a25623c89
codeaid-dev <codeaid.lab@gmail.com>
2021-07-12 23:52:13 +0900

    fix

--- Practice_fileio/json_decode.py
+++
@@ -1,5 +0,0 @@
-import json
-
-with open("test.json") as fh:
-    js = json.loads(fh.read())
-    print(js["hello"])
--- Practice_fileio/json_encode.py
+++
@@ -1,6 +0,0 @@
-import json
-
-dict = {"japanese": "日本語", "english": "英語"}
-text = json.dumps(dict, sort_keys=True, ensure_ascii=False, indent=2)
-with open("test.json", "w") as fh:
-    fh.write()
