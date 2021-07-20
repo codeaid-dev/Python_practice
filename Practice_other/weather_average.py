5204b5ab1a0f4da7a4d078533ed8ea0b676cab77
codeaid-dev <codeaid.lab@gmail.com>
2021-07-20 21:48:11 +0900

    fix

--- Practice_other/weather_average.py
+++ Practice_other/weather_average.py
@@ -1,21 +1,25 @@
 import csv
 import matplotlib.pyplot as plt
+from datetime import datetime
 
 filename = 'weather.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    averages = []
+    dates, averages = [], []
     for row in reader:
+        date = datetime.strptime(row[0], '%Y/%m/%d')
         average = float(row[1])
+        dates.append(date)
         averages.append(average)
 
 fig, ax = plt.subplots()
-ax.plot(averages)
-plt.title("Daily average temperatures, 2020-2021", fontsize=20)
+ax.plot(dates, averages)
+plt.title("Daily average Osaka temps, 2020-2021", fontsize=20)
 plt.xlabel("", fontsize=14)
 plt.ylabel("Temperature(C)", fontsize=14)
+fig.autofmt_xdate()
 ax.tick_params(labelsize=14)
-
+plt.grid()
 plt.show()
