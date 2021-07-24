91e4ca10b5319e4fbce3751a486ed2993c1bd441
codeaid-dev <codeaid.lab@gmail.com>
2021-07-24 12:27:26 +0900

    fix add

---
+++ Practice_other/python_repos.py
@@ -0,0 +1,41 @@
+import requests
+from plotly.graph_objs import Bar
+from plotly import offline
+
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+headers = {'Accept': 'application/vnd.github.v3+json'}
+r = requests.get(url, headers=headers)
+print(f'ステータスコード：{r.status_code}')
+
+response_dict = r.json()
+repo_dicts = response_dict["items"]
+repo_names, stars, labels = [], [], []
+for repo_dict in repo_dicts:
+    repo_names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+    owner = repo_dict['owner']['login']
+    description = repo_dict['description']
+    label = f'{owner}<br>{description}'
+    labels.append(label)
+
+data = [{
+    'type': 'bar',
+    'x': repo_names,
+    'y': stars,
+    'hovertext': labels
+}]
+
+layout = {
+    'title': 'Githubで最も多くのスターがついているPythonプロジェクト',
+    'titlefont': {'size': 28},
+    'xaxis': {
+        'title': 'リポジトリ',
+        'titlefont': {'size': 24}},
+    'yaxis': {
+        'title': 'スターの数',
+        'titlefont': {'size': 24}}
+}
+
+fig = {'data': data, 'layout': layout}
+offline.plot(fig, filename='python_repos.html')
