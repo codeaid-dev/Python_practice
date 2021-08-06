bb98b081e146a0242732c5f189c9d4fffcea15f6
codeaid-dev <codeaid.lab@gmail.com>
2021-08-06 10:11:25 +0900

    add game

---
+++ Practice_game/omikuji.py
@@ -0,0 +1,39 @@
+import tkinter
+import random
+
+omikuji = []
+
+def click_btn():
+    canvas.delete('omikuji')
+    result = random.randint(1,100)
+    if 1 <= result <= 2:
+        num = 0
+    elif 3 <= result <= 12:
+        num = 1
+    elif 13 <= result <= 32:
+        num = 2
+    elif 23 <= result <= 62:
+        num = 3
+    elif 53 <= result <= 82:
+        num = 4
+    elif 73 <= result <= 92:
+        num = 5
+    else:
+        num = 6
+
+    canvas.create_image(200,350,image=omikuji[num],tags='omikuji')
+
+root = tkinter.Tk()
+root.title('おみくじ')
+root.resizable(False, False)
+canvas = tkinter.Canvas(root, width=400, height=800)
+canvas.pack()
+box = tkinter.PhotoImage(file='omikuji.png')
+
+for i in range(1,8):
+    omikuji.append(tkinter.PhotoImage(file=f'omikuji_fuda{i}.png'))
+
+canvas.create_image(200,350,image=box,tags='omikuji')
+button = tkinter.Button(root, text='おみくじを引く', command=click_btn, font=('メイリオ', 36))
+button.pack(side=tkinter.BOTTOM, pady=50)
+root.mainloop()
