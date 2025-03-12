71612c827e328718175ca38559a8a243316d56d2
codeaid-dev <codeaid.lab@gmail.com>
2025-03-12 14:16:19 +0900

    add

---
+++ cli_game/find_missing_alph.py
@@ -0,0 +1,32 @@
+import random
+import time
+
+ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+seikai = random.choice(ALPHABET)
+question = ''
+for moji in ALPHABET:
+    if moji != seikai:
+        question += moji
+
+mode = random.randint(1,3)
+if mode == 1:
+    question = question[::-1]
+elif mode == 2:
+    temp = ''
+    while True:
+        a = random.choice(question)
+        if a not in temp:
+            temp += a
+        if len(temp) == 25:
+            break
+    question = temp
+
+print(question)
+start = time.time()
+answer = input('抜けているアルファベットは？')
+if answer.upper() == seikai:
+    print('正解です')
+    end = int(time.time()-start)
+    print(f'{end}秒かかりました')
+else:
+    print(f'間違いです(正解：{seikai})')
